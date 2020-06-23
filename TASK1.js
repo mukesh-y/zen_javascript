8bbaa3326336db7e5f605c33add09a5901f8b7f6
mukesh-y <67313780+mukesh-y@users.noreply.github.com>
2020-06-23 16:38:26 +0530

    Add files via upload

---
+++ TASK1.js
@@ -0,0 +1,70 @@
+//Task 1 :Question 1
+
+let a = "";
+let b = "";
+let c = "";
+let d = "";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+//Task 1 :Question 2
+
+var myvar= "1";
+console.log(myvar);
+
+//Task 1 :Question 3
+
+let fname = "";
+let lastname = "";
+let marstat = "";
+let country = "";
+let age = "";
+
+
+
+//Task 1 :Question 4
+
+var [fname1,lastnmae1,marsta,country1,age1] = [];
+
+//Task 1 :Question 5
+
+let string1 = "I love my country"
+let bool = true
+var undef ;
+let nullvar = null
+
+
+//Task 1 :Question 6
+
+let string_converted = parseInt(myvar);
+let strin_num = Number(string_converted);
+let string_con = ""+myvar;
+
+//Task 1 :Question 7
+
+
+let x;
+x = 1;   
+x = '1';
+x = [1]; 
+
+var t = (1 == '1');
+var t2 = (1 == [1]);
+var t3 = ('1' == [1]);
+console.log(t,t2,t3) // all true
+
+// all false
+var f3 = (1 === '1');
+var f = (1 === [1]);
+var f2 = ('1' === [1]); 
+console.log(f,f2,f3)// all false
+
+
+
+//~~~~~~~~~~~~~~~<-------------------END OF TASK NO 1----------------->~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
