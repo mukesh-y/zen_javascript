9b28fe629ca24f075ca1d36bfe42a5bd4469ead4
mukesh-y <67313780+mukesh-y@users.noreply.github.com>
2020-06-24 12:28:34 +0530

    Add files via upload

---
+++ celcius.js
@@ -0,0 +1,4 @@
+let celsius = prompt("Enter a celsius value: ");
+
+let fahrenheit = (celsius * 1.8) + 32
+console.log(" " +celsius+ "degree celsius is equal to" +fahrenheit+ " degree fahrenheit.");
