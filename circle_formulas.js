e8bf6dabbae286bf248fc4cb26bd7e352790e490
mukesh-y <67313780+mukesh-y@users.noreply.github.com>
2020-06-24 21:08:34 +0530

    Add files via upload

---
+++ circle_formulas.js
@@ -0,0 +1,9 @@
+let a = prompt("Enter Radius of  Circle");
+
+let dia = 2*a;
+let area = (22/7)*a*a
+let circumference = 2*(22/7)*a
+
+console.log("Diameter of the Circle is "+dia);
+console.log("Circumference of the Circle is "+circumference);
+console.log("Area of the Circle is "+area);
