3e3a600016f412f23924bea2835c605f039c1b57
mukesh-y <67313780+mukesh-y@users.noreply.github.com>
2020-06-24 17:24:17 +0530

    Add files via upload

---
+++ simpleintrestrate.js
@@ -0,0 +1,6 @@
+let p = prompt("Enter the Principal Amount :");
+let r = prompt("Enter the Rate of Intrest  :");
+let n = prompt("Enter the No. of Years  :");
+
+result = "The interest is " + (p*n*r/100);
+console.log(result)
