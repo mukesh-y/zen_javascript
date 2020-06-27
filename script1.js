a30993da6e94dc7f98e0c21f693990db304a2f51
mukesh-y <67313780+mukesh-y@users.noreply.github.com>
2020-06-27 08:42:26 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+alert("I'm JavaScript!");
+alert('Hello') // this line is not having semicolon but its working beacuse is a string
+alert(`Wor
+ ld`)
+alert(3 +
+1
++ 2); // this is multiple line code and its working properly beacuse of the parenthesis  amd returning 6
